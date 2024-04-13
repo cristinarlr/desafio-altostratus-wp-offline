3189d40598015d3abbc12cef0f9fe1a063f4a6f9
Cristina <cristinarlr@gmail.com>
2024-04-14 00:40:46 +0200

    keeping same user & psw for DB

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'my-database' );
 
 /** Database username */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', 'wp-password42' );
 
 /** Database hostname */
 define( 'DB_HOST', '35.193.29.174' );
